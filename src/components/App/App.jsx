b572d1824aec8946aaa17c007249898076bbeba7
Touathi Vang <Touathi@outlook.com>
2023-06-09 16:28:57 -0500

    added a feedback component

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -5,6 +5,7 @@ import Feelings from '../Feelings/Feelings';
 import Understanding from '../Understanding/Understanding';
 import Support from '../Support/Support';
 import Comments from '../Comments/Comments';
+import Feedback from '../Feedback/Feedback';
 import { HashRouter as Router, Route, Link } from 'react-router-dom';
 
 
@@ -13,7 +14,7 @@ function App() {
   return (
     <Router>
       <div className='App'>
-        
+
         <header className='App-header'>
           <h1 className='App-title'>Feedback!</h1>
           <h4>Don't forget it!</h4>
@@ -37,6 +38,11 @@ function App() {
         <Route exact path="/page-3">
           {/* Page four */}
           <Comments />
+        </Route>
+
+        <Route exact path="/page-4">
+          {/* Page 5/ Review Your Feedback */}
+          <Feedback />
         </Route>
 
       </div>
---
+++ src/components/Feedback/Feedback.jsx
@@ -0,0 +1,39 @@
+
+
+function Feedback() {
+
+
+    return (
+        <>
+
+        <h2>Review Your Feedback</h2>
+
+        <br></br><br></br>
+       
+
+        {/* grab data Feelings from server */}
+        <p>Feelings: </p>
+
+        <br></br><br></br>
+        
+
+        {/* grab data Understanding from server */}
+        <p>Understanding: </p>
+
+        <br></br><br></br>
+        
+
+        {/* grab data Support from server */}
+        <p>Support: </p>
+
+        <br></br><br></br>
+        
+
+        {/* grab comments from server */}
+        <p>Comments: </p>
+
+        </>
+    )
+}
+
+export default Feedback
