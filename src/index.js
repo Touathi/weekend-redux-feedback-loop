cc9c883f9c36603d6fc75d8530fcd84471e7d85b
Touathi Vang <Touathi@outlook.com>
2023-06-09 22:24:13 -0500

    add some reducer

--- src/index.js
+++ src/index.js
@@ -7,18 +7,85 @@ import { applyMiddleware, combineReducers, createStore } from 'redux';
 import logger from 'redux-logger';
 
 
+// feeling reducer data
+const feeling = (state = [], action) => {
+    
+    if (action.type === 'GET_FEELING') {
+        return action.payload
+    } 
+        else if (action.type === 'GET_SUBMIT') {
+
+            return state = []
+    }
+
+    return state;
+}
+
+// understanding reducer data
+const understanding = (state = [], action) => {
+
+    if (action.type === 'GET_UNDERSTANDING') {
+        return action.payload
+    } 
+        else if (action.type === 'GET_SUBMIT') {
+
+            return state = []
+    }
+
+    return state;
+}
+
+// support reducer data
+const support = (state = [], action) => {
+
+    if (action.type === 'GET_SUPPORT') {
+        return action.payload
+    } 
+        else if (action.type === 'GET_SUBMIT') {
+
+            return state = []
+    }
+
+    return state;
+}
+
+// comment reducer data
+const comment = (state = [], action) => {
+
+    if (action.type === 'GET_COMMENT') {
+        return action.payload
+    } 
+        else if (action.type === 'GET_SUBMIT') {
+
+            return state = []
+    }
+
+    return state
+}
+
+// FeedbackData reducer from server
 const feedbackData = (state = [], action) => {
 
     if (action.type === 'GET_FEEDBACK') {
         return action.payload
+    } 
+        else if (action.type === 'GET_SUBMIT') {
+
+            return state = []
     }
     
     return state;
 }
 
+
+// store
 const reduxStore = createStore(
     combineReducers({
-        feedbackData
+        feedbackData,
+        feeling,
+        understanding,
+        support,
+        comment
     })
 )
 
